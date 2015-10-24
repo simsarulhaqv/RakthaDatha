5b4f915e14a77b71d86bc069513ec42cc17c5bf1
Shrimadhav U K <magicteacher95@gmail.com>
2015-10-24 20:46:46 +0530

    add goofle chart api

--- www/global.php
+++ www/global.php
@@ -5,5 +5,5 @@ date_default_timezone_set('Asia/Calcutta');
 setlocale(LC_MONETARY, 'en_IN');
 require_once("connection.php");
 //require_once("functions.php");
-
+ini_set('display_errors', 1); error_reporting(-1);
 ?>
